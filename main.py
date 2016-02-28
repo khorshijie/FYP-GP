f9bc8dd0e864c63b20fc62997311015b3ef6444d
Khor Shi-Jie <khorshijie@gmail.com>
2016-02-28 11:35:06 +0800

    using GPy

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import read_input
 import gaussian_process
+import GPy
+from matplotlib import pyplot as plt
 
 # options for kernels: WN, C, LIN, SE, PER
 
 if __name__ == "__main__":
 	reader = read_input.InputReader("data.txt")
-	X,y = reader.get_data()
+	X,y = reader.get_data("GPy")
 	print X, y
-	gp = gaussian_process.GaussianProcess(X, y, "LIN", [3, 0], 0.00001)
-	print gp.predict(9)
 
+	# Specify linear kernel
+	kernel = GPy.kern.Linear(1)
+
+	# Create GP model
+	m = GPy.models.GPRegression(X,y,kernel)
+	m.optimize()
+	m.optimize_restarts(num_restarts = 10)
+	m.plot()
+	
+	plt.show()
--- read_input.py
+++ read_input.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 class InputReader:
 
 	def __init__(self, file_name):
 		self.file_name = file_name
 
-	def get_data(self):
+	def get_data(self, option):
 		X = []
 		y = []
 		with open(self.file_name) as f:
@@ -11,11 +13,14 @@ class InputReader:
 		for s in content:
 			args = s.split();
 			X.append(args[:-1])
-			y.append(int(args[-1]))
+			y.append(np.asarray(int(args[-1])))
 		X = [[int(x) for x in lst] for lst in X]
 		
-		# for scalar inputs
-		X = [item for sublist in X for item in sublist]
+		if option == "scalar":
+			X = [item for sublist in X for item in sublist]
+		elif option == "GPy":
+			X = np.asarray(X)
+			y = np.transpose(np.asmatrix(y))
 
 		return X, y
 
